05a56f817e1490b87455956245cc41b9bd3f6c64
Anastasia Shtrezer <shtrezer957@gmail.com>
2019-09-19 00:19:08 +0300

    Color

--- app/src/main/java/com/example/language/MainActivity.java
+++ app/src/main/java/com/example/language/MainActivity.java
@@ -2,37 +2,74 @@ package com.example.language;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.LayoutDirection;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.security.cert.Extension;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int n;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Utils.onActivityCreateSetTheme(this);
         setContentView(R.layout.activity_main);
+
     }
 
     public void btnOk(View view) {
         Locale locale;
-        Spinner spinner = findViewById(R.id.spinner);
+        Spinner spinner = findViewById(R.id.spinnerLanguage);
         int position = spinner.getSelectedItemPosition();
         if (position == 1) {
             locale = new Locale("ru");
         } else {
             locale = new Locale("eng");
         }
+
+        Spinner colorSinner = findViewById(R.id.spinnerColor);
+        Utils.changeToTheme(this, colorSinner.getSelectedItemPosition());
+
+
         Configuration config = new Configuration();
         config.setLocale(locale);
         getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
         recreate();
+
+
+    }
+
+    public void onClick(View v) {
+        switch (this.n) {
+            case 0:
+                Utils.changeToTheme(this, Utils.THEME_BLACK);
+                this.n = 1;
+                return;
+            case 1:
+                Utils.changeToTheme(this, Utils.THEME_GREEN);
+                this.n = 2;
+                return;
+            case 2:
+                Utils.changeToTheme(this, Utils.THEME_BLUE);
+                this.n = 3;
+                return;
+
+            default:
+                break;
+
+
+        }
 
     }
 
---
+++ app/src/main/java/com/example/language/Utils.java
@@ -0,0 +1,48 @@
+package com.example.language;
+
+
+import android.app.Activity;
+import android.content.Intent;
+
+public class Utils
+{
+    private static int sTheme;
+
+    public final static int THEME_DEFAULT =300;
+    public final static int THEME_GREEN = 0;
+    public final static int THEME_BLUE = 1;
+    public final static int THEME_BLACK = 2;
+
+    /**
+     * Set the theme of the Activity, and restart it by creating a new Activity of the same type.
+     */
+    public static void changeToTheme(Activity activity, int theme)
+    {
+        sTheme = theme;
+        activity.finish();
+
+        activity.startActivity(new Intent(activity, activity.getClass()));
+
+    }
+
+    /** Set the theme of the activity, according to the configuration. */
+    public static void onActivityCreateSetTheme(Activity activity)
+    {
+        switch (sTheme)
+        {
+            default:
+            case THEME_DEFAULT:
+                activity.setTheme(R.style.AppTheme);
+                break;
+            case THEME_GREEN:
+                activity.setTheme(R.style.AppThemeGreen);
+                break;
+            case THEME_BLUE:
+                activity.setTheme(R.style.AppThemeBlue);
+                break;
+            case THEME_BLACK:
+                activity.setTheme(R.style.AppThemeBlack);
+                break;
+        }
+    }
+}
